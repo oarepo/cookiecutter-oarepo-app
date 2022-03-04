6ab7ddf46046994f27a59af36d8cb68dfeae6bb8
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-03-04 17:18:06 +0100

    fix jinja

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/theme/assets/semantic-ui/js/{{cookiecutter.package_name}}/landing_page/RecordManagement.js
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/theme/assets/semantic-ui/js/{{cookiecutter.package_name}}/landing_page/RecordManagement.js
@@ -5,7 +5,7 @@
 //
 // Invenio RDM Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
-
+{%- raw %}
 import React, { useState } from 'react'
 import { Grid, Message, Divider } from 'semantic-ui-react'
 
@@ -55,3 +55,4 @@ export const RecordManagement = (props) => {
     </Grid>
   )
 }
+{%- endraw %}
